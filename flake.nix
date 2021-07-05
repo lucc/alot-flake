1dbb940218186ac76c252147a5096b62eb3a7182
Lucas Hoffmann <lucc@posteo.de>
2021-07-05 12:55:33 +0200

    Only use a dev env

--- flake.nix
+++ flake.nix
@@ -1,33 +1,11 @@
 {
-  inputs = {
-    nixpkgs.url = "flake:sys";
-    flake-utils.url = "github:numtide/flake-utils";
-    alot-sources = {
-      url = "path:/home/luc/src/alot";
-      flake = false;
-    };
-  };
-
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  inputs.nixpkgs.url = "flake:sys";
+  outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in
   {
-    overlay = final: prev:
-      let
-        pkgs = nixpkgs.legacyPackages.${prev.system};
-      in
-      rec {
-        alot = pkgs.alot.overridePythonAttrs (oa: {
-          version = "dev";
-          src = inputs.alot-sources;
-          propagatedBuildInputs = oa.propagatedBuildInputs ++ (
-            with pkgs.python3Packages; [notmuch2 cffi]
-            );
-        });
-      };
-
     devShell.${system} = pkgs.mkShell {
       buildInputs = with pkgs; [
         (python3.withPackages(ps: with ps; [
@@ -46,45 +24,7 @@
         awesome
         dbus
       ];
+      PYTHONPATH = "/home/luc/src/alot";
     };
-
-    #defaultPackage.${system} = pkgs.python.pkgs.buildPythonPackage rec {
-    #  pname = "alot";
-    #  version = "dev";
-    #  src = ./.;
-    #  postPatch = ''
-    #    substituteInPlace alot/settings/manager.py --replace /usr/share "$out/share"
-    #  '';
-
-    #  nativeBuildInputs =  pkgs.sphinx;
-
-    #  propagatedBuildInputs = with pkgs; [
-    #    notmuch
-    #    urwid
-    #    urwidtrees
-    #    twisted
-    #    python_magic
-    #    configobj
-    #    service-identity
-    #    file
-    #    gpgme
-    #  ];
-    #}
-    #;
-    #defaultApp.${system} = { type = "app"; program = "${self}/bin/alot"; };
-  } //
-  flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ self.overlay ];
-          inherit system;
-        };
-      in
-      rec {
-        packages = { inherit (pkgs.alot); };
-        defaultPackage = pkgs.alot;
-        apps.alot = flake-utils.lib.mkApp { drv = pkgs.alot; name = "alot"; };
-        defaultApp = apps.alot;
-      }
-  );
+  };
 }
